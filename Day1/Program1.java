c60379665a1fa4123683235d2a62a0863eee6368
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-08-26 20:05:53 +0530

    Day1 assignments

---
+++ Day1/Program1.java
@@ -0,0 +1,18 @@
+class Program1{
+   	public static void main(String args[]){
+  
+int a = -10;
+if(a==0)
+{
+   System.out.println("number is Zero");
+}
+else if(a>0)
+{
+   System.out.println("number is Positive");
+}
+else
+{
+   System.out.println("number is Negative");
+  }
+ }
+}
---
+++ Day1/Program4.java
@@ -0,0 +1,10 @@
+class Program4{
+
+	public static void main(String args[]){
+
+	for(int i=15; i>=0; i--)
+	{
+    	   System.out.print(i+" ");
+  	}
+    }
+}

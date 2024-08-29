50032d8cc01f2e0528702e3a676731acb06c68e1
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-08-29 22:30:41 +0530

    Day4 assignments

---
+++ Day4/Program1.java
@@ -0,0 +1,11 @@
+class Program1{
+	public static void main(String args[]){
+
+	char a = 'A';
+	for(char i=1; i<=5; i++)
+	{
+	  System.out.print(a + " ");
+	  a += 2;
+	}
+    }
+}
---
+++ Day4/Program2.java
@@ -0,0 +1,17 @@
+class Program2{
+	public static void main(String args[]){
+
+	int i;
+	for(i=1; i<=5; i++)
+	{
+	  if(i%2==1)
+	{
+	  System.out.print(i*i*i+" ");
+	}
+	else
+	{
+	  System.out.print(i*i+" ");
+	}
+      }	
+   }
+}

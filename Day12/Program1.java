30b64918602f327fb9529e25080284418e30dfe1
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-09-18 20:34:05 +0530

    Day12 assignments

---
+++ Day12/Program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Program1{
+	public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+
+	System.out.println("Addition is: "+(a+b));
+	System.out.println("Subtraction is: "+(b-a));
+	}
+}
---
+++ Day12/Program3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Program3{
+	public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+
+	System.out.println("Addition of 1000 & 8000 is: "+(a*a*a+b*b*b));
+	System.out.println("Subtraction of 100 & 400: "+(a*a-b*b));
+	}
+}

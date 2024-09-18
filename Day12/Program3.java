27277d3cf470f3d2189105a06cac41c2f0a2edea
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-09-18 20:37:59 +0530

    Day12 assignments

--- Day12/Program3.java
+++ Day12/Program3.java
@@ -7,7 +7,7 @@ class Program3{
 	int a = sc.nextInt();
 	int b = sc.nextInt();
 
-	System.out.println("Addition of 1000 & 8000 is: "+(a*a*a+b*b*b));
-	System.out.println("Subtraction of 100 & 400: "+(a*a-b*b));
+	System.out.println("Addition of 1000 & 8000 is: "+((a*a*a)+(b*b*b));
+	System.out.println("Subtraction of 100 & 400: "+((a*a)-(b*b)));
 	}
 }

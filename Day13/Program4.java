3d650a0d1213466db2f54439063d44117f260f83
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-09-24 21:00:02 +0530

    Day13 assignments

---
+++ Day13/Program4.java
@@ -0,0 +1,13 @@
+class Program4{
+	public static void main(String args[]){
+
+	int num = 5;
+	int fact = 1;
+	
+	for(int i=1; i<=num; i++)
+
+	fact = fact*i;
+	System.out.println("The Factorial of "+ num +" is: "+fact);
+	}
+}
+	
---
+++ Day13/Program5.java
@@ -0,0 +1,13 @@
+class Program5{
+	public static void main(String args[]){
+
+	int arr[] = {10,20,30,40,50};
+
+	System.out.print("array elements:");
+
+	for(int i=0; i<arr.length; i++)
+	{
+		System.out.print(arr[i]+" ");
+	}
+    }
+}

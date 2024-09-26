dbae49838bd47c7cc3a4cfc5cd2a632b0572f321
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-09-26 21:05:19 +0530

    Day15 assignments

---
+++ Day15/Program5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Program5{
+
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Enter three numbers: ");
+	int n1 = sc.nextInt();
+	int n2 = sc.nextInt();
+	int n3 = sc.nextInt();
+
+
+	if(n1>=n2 && n1>=n3)
+	{
+	   System.out.print("The Maximum number amongst "+ n1 +" "+ n2 +" & "+ n3 +" is"+": "+ n1);
+	}
+	else if(n2>=n1 && n2>=n3)
+	{
+	   System.out.print("The Maximum number amongst "+ n1 +" "+ n2 +" & "+ n3 +" is"+": "+ n2);
+	}
+	else
+	{
+	   System.out.print("The Maximum number amongst "+ n1 +" "+ n2 +" & "+ n3 +" is"+": "+ n3);
+	}
+    }
+}

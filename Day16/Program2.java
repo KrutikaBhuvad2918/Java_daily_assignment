49efeb295053f9505f98e63e5c883dd51a3fe29b
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-10-01 22:38:23 +0530

    Day16 assignments

--- Day16/Program2.java
+++ Day16/Program2.java
@@ -3,12 +3,15 @@ class Program2{
 	public static void main(String args[]){
 	Scanner sc = new Scanner(System.in);
 
-	System.out.println("Enter number: ");
-	int n = sc.nextInt();
+	System.out.println("Enter num1: ");
+	int n1 = sc.nextInt();
+
+	System.out.println("Enter num2: ");
+	int n2 = sc.nextInt();
 
 	int fact = 1;
 
-	for(int i=1; i<=n; i++)
+	for(int i=n1; i<=n2; i++)
 	{
 	   System.out.println("Factorial of " + i + " = "+ (fact = fact*i));
 	}

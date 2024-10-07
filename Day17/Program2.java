820f4171d76022aec649fd9196a06a5603016395
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-10-07 23:49:39 +0530

    Day17 assignments

--- Day17/Program2.java
+++ Day17/Program2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class Program2{
 	public static void main(String args[]){
 	Scanner sc = new Scanner(System.in);
@@ -6,8 +7,8 @@ class Program2{
 	System.out.print("Enter number: ");
 	int num = sc.nextInt();
 	
-	System.out.println("Second Predecessor: "+ (num-2));
-	System.out.println("Second Successor: "+ (num+2));
+	System.out.println("Second Predecessor: " + (num-2));
+	System.out.println("Second Successor: " + (num+2));
        }
 }
 
--- Day17/Program3.java
+++ Day17/Program3.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class Program3{
 	public static void main(String args[]){
 	Scanner sc = new Scanner(System.in);

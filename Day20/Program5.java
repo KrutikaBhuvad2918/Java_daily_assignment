3517c2f2c76256386001258c3cf28ed3404b6c41
KrutikaBhuvad <krutikabhuvad2004@gmail.com>
2024-10-09 15:33:11 +0530

    Day20 assignments

--- Day20/Program5.java
+++ Day20/Program5.java
@@ -1,28 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Program5{
-	public static void main(String args[]){
-	Scanner sc = new Scanner(System.in);
-	
-	int max = 0;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-	int arr[] = new int[5];
+        System.out.print("Enter the number of elements in the array = ");
+        int n = sc.nextInt();
 
-	System.out.println("Enter array elements: ");
-	for(int i=0; i<arr.length; i++)
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements = ");
+
+        for (int i = 0; i < n; i++) 
 	{
-	   arr[i] = sc.nextInt();
-	   
-	   max = arr[0];
-	}
-	for(int i=0; i<arr.length; i++)
+           arr[i] = sc.nextInt();
+        }
+	int largest = arr[0];
+
+        for (int i = 1; i < n; i++) 
 	{
-	 if(max < arr[i])
-	 {
-	    max = arr[i];
-	 }
-	}
-	System.out.println("Largest Element = "+ max);
+        if (arr[i] > largest) 
+	{
+            largest = arr[i];
+        }
+      }
+	System.out.println("The largest element in the array is = " + largest);
     }
 }
-	
